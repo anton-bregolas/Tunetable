c71f6958f7219a46977b480e6560e547eda0a756
anton-bregolas <106439112+anton-bregolas@users.noreply.github.com>
2023-08-12 00:31:15 +0300

    TSO Tunelist Converter v.0.7: Sort function and menu revised, new  buttons added

--- tso-tunelist-converter.js
+++ tso-tunelist-converter.js
@@ -1,4 +1,6 @@
+////////////////////////////////////////////////
 // Declare global variables, create local JSONs
+///////////////////////////////////////////////
 
 let noThe = 1;
 let tDelay = 0;
@@ -8,14 +10,18 @@ let sortedJson = {};
 
 const tuneTable = document.querySelector('#t-tunes');
 const infoBox = document.querySelector('.info-box');
+const radioBox = document.querySelector('.radio-container');
 const inputForm = document.querySelector('#input-form');
 const saveBtn = document.querySelector('.n-save-btn');
 const helpBtn = document.querySelector('.n-help-btn');
 const themeBtn = document.querySelector('.n-theme-btn');
+const revertBtn = document.querySelector('.t-revert-btn');
 const saveIcon = saveBtn.querySelector('.n-save-icon');
 const helpIcon = helpBtn.querySelector('.n-help-icon');
 const sunIcon = themeBtn.querySelector('.n-theme-icon-sun');
 const moonIcon = themeBtn.querySelector('.n-theme-icon-moon');
+const exampleTunebook = document.querySelector('.example-tunebook');
+const exampleTunelist = document.querySelector('.example-tunelist');
 const inputLabel = document.querySelector('.input-label');
 
 // Display Infobox message or warning
@@ -93,7 +99,8 @@ async function fetchTheSessionJson(url) {
       const { id, name, url, type } = tune;
       importJson.tunes.push({ id, name, url, type });
     } 
-
+    
+    const timeStamp1 = new Date();
     tDelay = tsoData.pages > 1 ? Math.floor((+tsoData.pages - 1) / 2.6 * 1000) : 0;
     console.log(`Table processing delay estimated at ${tDelay} ms`);
 
@@ -139,6 +146,8 @@ async function fetchTheSessionJson(url) {
       await Promise.all([messagePromise, fetchingPromise]);
     }
 
+    const timeStamp2 = new Date();
+    console.log(`Estimated delay: ${tDelay}ms, actual delay: ${timeStamp2 - timeStamp1}ms`);
     createTuneTable(importJson);
 
     inputForm.value = "";
@@ -174,27 +183,29 @@ function createTuneTable(myJson) {
 
     for (let j = 0; j < 4; j++) {
 
-      let cellId = j == 0? (i + 1) : 
-      j == 1? myTunes[i].name : 
-      j == 2? myTunes[i].id : myTunes[i].url;
-
       const tuneCell = document.createElement("td");
-      const tuneText = document.createTextNode(cellId);
       const cellSpan = document.createElement("span");
+      cellSpan.classList.add("t-cell");
 
-      tuneCell.appendChild(cellSpan);
-
-      cellSpan.setAttribute("class", "t-cell");
-
+      let cellContent =
+        j === 0 ? i + 1 :
+        j === 1 ? myTunes[i].name :
+        j === 2 ? myTunes[i].id : myTunes[i].url;
+      
       if (j === 3) {
+
         const cellLink = document.createElement("a");
-        cellLink.setAttribute("href", cellId);
+        cellLink.setAttribute("href", cellContent);
         cellLink.setAttribute("target", "_blank");
-        cellSpan.appendChild(cellLink).appendChild(tuneText);
+        cellLink.textContent = cellContent;
+        cellSpan.appendChild(cellLink);
+      
       } else {
-        cellSpan.appendChild(tuneText);
+
+        cellSpan.textContent = cellContent;
       }
 
+      tuneCell.appendChild(cellSpan);
       tuneRow.appendChild(tuneCell);
     }
 
@@ -203,23 +214,11 @@ function createTuneTable(myJson) {
 
   showInfoMsg("Hup!");
   console.log('Tunetable created');
-
 }
 
-// Check if a nested JSON array of tunes is empty
+// Check if a nested JSON array of tunes is missing or empty
 
 function checkIfEmptyJson(checkJson) {
-
-  // Check tunelist object via JSON.stringify (slow)...
-  
-  // let checkString = JSON.stringify(checkJson);
-
-  // if (checkString == "{}") {
-  //   return true;
-  // } 
-  // return false;
-
-  // Check nested tunes array via array.length (faster)...
 
   if (Array.isArray(checkJson.tunes) && checkJson.tunes.length) { 
 
@@ -241,6 +240,8 @@ function createDeepCopyJson(rawJson) {
 // After it moves or cuts articles, reorder tunes by name and return sorted JSON.
 
 function sortTunetable(myJson) {
+
+  console.log("Sorting tune JSON");
 
   sortedJson = createDeepCopyJson(myJson);
 
@@ -337,7 +338,28 @@ function clearTunetable() {
 
 }
 
+// Show or hide help menu
+
+function toggleHelpMenu() {
+  
+  hideSortMenu();
+  helpIcon.classList.toggle("active");
+  inputLabel.classList.toggle("displayed");
+}
+
+// Hide Sort options menu
+
+function hideSortMenu() {
+
+  if (radioBox.classList.contains("displayed-flex")) {
+    
+    radioBox.classList.remove("displayed-flex");
+  }
+}
+
+//////////////////////////////////////////////
 // Add listeners to buttons and radio buttons
+/////////////////////////////////////////////
 
 function initButtons() {
 
@@ -346,6 +368,8 @@ function initButtons() {
   generateTunetableBtn.addEventListener('click', (event) => {
 
     event.preventDefault();
+
+    hideSortMenu();
 
     if (inputForm.value) {
 
@@ -369,6 +393,7 @@ function initButtons() {
 
     event.preventDefault();
 
+    hideSortMenu();
     clearTunetable();
     clearJsonData();
     
@@ -380,15 +405,26 @@ function initButtons() {
 
     event.preventDefault();
 
-    if (checkIfEmptyJson(importJson)) {
-
-      showInfoMsg("No tunes to sort!", 1);
-      return;
+    if (inputLabel.classList.contains("displayed")) {
+      toggleHelpMenu();
     }
 
-    createTuneTable(sortTunetable(importJson));
+    if (checkIfEmptyJson(importJson)) {
 
-    showInfoMsg("Sorted!");
+      return showInfoMsg("No tunes to sort!", 1);
+
+    } else if (radioBox.classList.contains("displayed-flex")) {
+
+      hideSortMenu();
+      createTuneTable(sortTunetable(importJson));
+      showInfoMsg("Sorted!");
+      return;
+
+    } else {
+
+      radioBox.classList.add("displayed-flex");
+      return;
+    }
 
   });
 
@@ -401,10 +437,17 @@ function initButtons() {
     });
   }
 
-  const tuneTableHeaderBtn = document.querySelector("#t-headers");
-  const tuneCells = document.querySelector("#t-tunes");
+  const hideSortMenuBtn = document.querySelector('.r-hide-sort-btn');
 
-  tuneTableHeaderBtn.addEventListener('click', () => {
+  hideSortMenuBtn.addEventListener('click', () => {
+
+    hideSortMenu();
+  });
+
+  const tuneCells = document.querySelector("#t-tunes");
+  const tuneTableUrlBtn = document.querySelector("#t-head-url");
+
+  tuneTableUrlBtn.addEventListener('click', () => {
 
     if (window.screen.width >= 720) {
       tuneCells.querySelectorAll(".t-cell").forEach(cell => {
@@ -442,13 +485,35 @@ function initButtons() {
 
   helpBtn.addEventListener('click', () => {
 
-    helpIcon.classList.toggle("active");
-    inputLabel.classList.toggle("displayed");
-  })
+    toggleHelpMenu();
+  });
+
+  exampleTunebook.addEventListener('click', () => {
+
+    inputForm.value = "https://thesession.org/members/1/tunebook";
+    toggleHelpMenu();
+  });
+
+  exampleTunelist.addEventListener('click', () => {
+
+    inputForm.value = "https://thesession.org/members/1/tags/SliabhLuachra/tunes";
+    toggleHelpMenu();
+  });
+
+  revertBtn.addEventListener('click', () => {
+
+    if (!checkIfEmptyJson(importJson)) {
+
+      createTuneTable(importJson);
+      showInfoMsg("Reverted to TSO defaults!");
+    }
+  });
 
 }
 
+//////////////////////////////////////////////
 // Initiate necessary functions on page load
+/////////////////////////////////////////////
 
 document.addEventListener("DOMContentLoaded", () => {
   
