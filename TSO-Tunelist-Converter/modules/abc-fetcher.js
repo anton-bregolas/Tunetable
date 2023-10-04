a2d2f069ff17ac0e138c387f6a8dff88cd22b7cd
anton-bregolas <106439112+anton-bregolas@users.noreply.github.com>
2023-10-04 13:44:19 +0300

    v.1.9.0: Progressive Web App & major refactoring

--- TSO-Tunelist-Converter/modules/abc-fetcher.js
+++ TSO-Tunelist-Converter/modules/abc-fetcher.js
@@ -319,11 +319,11 @@ export async function loadAbcIncipits(importJson) {
     
     if (checkIfJsonHasTunes(importJson)) {
 
-      abcUrl = "https://raw.githubusercontent.com/anton-bregolas/TSO-Tunelist-Converter/deploy/abc.json";
+      abcUrl = "https://raw.githubusercontent.com/anton-bregolas/Tunetable/deploy/abc.json";
 
     } else if (checkIfJsonHasSets(importJson)) {
 
-      abcUrl = "https://raw.githubusercontent.com/anton-bregolas/TSO-Tunelist-Converter/deploy/abc-settings.json"
+      abcUrl = "https://raw.githubusercontent.com/anton-bregolas/Tunetable/deploy/abc-settings.json"
     }
 
     showInfoMsg("Loading ABC incipits...");
